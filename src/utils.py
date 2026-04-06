055f15174addccb599197aceb29ef1bc6e5ac4bc
Mahesh Chandrawanshi <chandrawanshimahesh01@gmail.com>
2026-04-06 10:38:03 +0530

    Data transformation is completed.

--- src/utils.py
+++ src/utils.py
@@ -20,3 +20,34 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
+    try:
+        report = {}
+        trained_models = {}
+
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=5)
+            gs.fit(x_train, y_train)
+
+            best_model = gs.best_estimator_   # ✅ IMPORTANT
+
+            best_model.fit(x_train, y_train)
+
+            y_test_pred = best_model.predict(x_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+            trained_models[model_name] = best_model  # ✅ store trained model
+
+        return report, trained_models
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
